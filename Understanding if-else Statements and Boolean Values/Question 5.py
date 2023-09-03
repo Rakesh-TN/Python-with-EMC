623f4c134169c33dc770550f06e819821a173d97
Rakesh-TN <tnrakesh68@outlook.com>
2023-09-03 14:48:21 +0530

    Qustion 4 completed

---
+++ Understanding if-else Statements and Boolean Values/Question 5.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the Your Score : "))
+if (a<35):
+    print("Poor Student")
+elif (a>35 and a<70):
+    print("Average Student")
+elif (a>70 and a<100):
+    print("Good Student")
+else:
+    print("Invaild Score")
