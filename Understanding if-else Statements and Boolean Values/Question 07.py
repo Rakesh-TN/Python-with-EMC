efaff57c9823b65180a19da0935dfbafee4aff99
Rakesh-TN <tnrakesh68@outlook.com>
2023-09-03 16:52:27 +0530

    Using Or

---
+++ Understanding if-else Statements and Boolean Values/Question 07.py
@@ -0,0 +1,6 @@
+salary = int(input("Enter the Salary : "))
+age = int(input("Enter the Age : "))
+if  (salary>=20000 or age<=25):
+    print("You're Eligible for Loan")
+else:
+    print("You're Eligible for Loan")
