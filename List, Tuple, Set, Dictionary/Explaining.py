507c07d7265718777431072745b6490641fd2606
Rakesh-TN <tnrakesh68@outlook.com>
2023-09-18 13:34:39 +0530

    List, Tuple, Set, Dictionary
    Signed-off-by: Rakesh-TN <tnrakesh68@outlook.com>

---
+++ List, Tuple, Set, Dictionary/Explaining.py
@@ -0,0 +1,26 @@
+# List
+a = [1,2,3,4,5,6,7]
+print(a)
+a.append(8)
+a.append(9)
+a.append(10)
+print(a)
+a.insert(0,20)
+print(a)
+a.pop(0) #Index Value
+print(a)
+
+#Tuple
+print("Tuple")
+a=(1,2,3,4,5,6)
+b=list(a)
+print(a)
+print(b)
+
+#Set
+print("Set")
+a= {1, 2, 3, 4, 5, 6}
+print(a)
+a.add(7)
+print(a)
+
